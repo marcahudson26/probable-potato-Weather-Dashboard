552d58fbb7258c30c9bea2d1a2ac05608103c366
marc hudson <marchudson2601@gmail.com>
2023-02-02 14:04:05 +0000

    h

--- index.js
+++ index.js
@@ -198,20 +198,3 @@ clearButton.addEventListener('click', (event) => {
 
 getHistory();
 
-function getLocation(place) {
-    return fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${place}&type=city&format=json&apiKey=${apiKey}`)
-        .then(response => response.json())
-        .then(result => result.results[0].place_id);
-}
-
-function getRestaurants(placeId) {
-    return fetch(`https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=place:${placeId}&limit=20&apiKey=${apiKey}`)
-        .then(response => response.json())
-        .then(result => result.features.map(x => x.properties.name).filter(x => x))
-}
-
-getLocation(userInput).then(placeId => {
-    getRestaurants(placeId).then(restaurants => {
-        console.log(restaurants)
-    })
-})
